8dc2c86a5a4d592fd83d3c1f599ab00c16bbcefa
Vikas Rv <vikasrvs@gmail.com>
2021-04-23 00:07:27 +0530

    Minor changes to frontend

--- ecom_app/core/views.py
+++ ecom_app/core/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 import datetime
 from .models import Product
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
+
 prods = Product.objects.all()
 def home_view(request):
+    try:
+        just_logged_out = request.session.get('just_logged_out',False)
+    except:
+        just_logged_out = False
     profile = User.objects.filter(username = request.user.username).first()
-    return render(request, 'core/home.html', context = {"prods": prods})
+    return render(request, 'core/home.html', context = {"prods": prods, "just_logged_out": just_logged_out})
 
 def about_view(request):
     return render(request, 'core/about.html', context = {"title": "About"})
+
+def logout_view(request):
+    logout(request)
