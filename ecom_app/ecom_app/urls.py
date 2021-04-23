b7f7fadcfc47f10125f06f3c6ffda0879a8c2320
Kushaal Tummala <tummala.kushaal01@gmail.com>
2021-04-23 10:30:50 +0530

    added login again btn after logout

--- ecom_app/ecom_app/urls.py
+++ ecom_app/ecom_app/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', include(core.urls)),
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
-    path('logout/', LogoutView.as_view(template_name="core/home.html"), name='logout'),
+    path('logout/', LogoutView.as_view(template_name="users/logout.html"), name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
