8fb1ba96d5033d092b549b66213a50bb620e1298
Vikas Rv <vikasrvs@gmail.com>
2021-04-25 01:59:43 +0530

    Add profile update

--- ecom_app/users/models.py
+++ ecom_app/users/models.py
@@ -13,5 +13,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.CharField(max_length=20, choices = USER_TYPES, default = "CUSTOMER")
     location = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
     def __str__(self):
         return f'{self.user.username} Profile'
--- ecom_app/users/urls.py
+++ ecom_app/users/urls.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import updateProfile
+from django.conf.urls.static import static
+from ecom_app import settings
+urlpatterns = [
+    path('', updateProfile, name="profile-update"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
