74abb74468989a541afa676ad23a0c626ac6a634
Vikas Rv <vikasrvs@gmail.com>
2021-04-24 19:33:03 +0530

    add checkout page

--- ecom_app/core/urls.py
+++ ecom_app/core/urls.py
@@ -9,7 +9,9 @@ from core.views import (
     ProductDeleteView,
     ProductUpdateView,
     ProductDetailView,
-    search
+    search,
+    checkout,
+    address_helper,
     )
 urlpatterns = [
     path('', ProductListView.as_view(), name = "app-home"),
@@ -19,5 +21,7 @@ urlpatterns = [
     path('product/new/', ProductCreateView.as_view(), name='prod-create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='prod-update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(template_name = 'core/prod_confirm_delete.html'), name='prod-delete'),
-    path('search', search)
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('search', search),
+    path('checkout', checkout),
+    path('address_help', address_helper),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
