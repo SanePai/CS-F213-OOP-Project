30103bb6dd25fefee85337e894192884196e04e9
Kushaal Tummala <tummala.kushaal01@gmail.com>
2021-04-24 18:05:52 +0530

    order

--- ecom_app/core/models.py
+++ ecom_app/core/models.py
@@ -26,7 +26,7 @@ UNIT_CHOICES = (
 class Product(models.Model):
     seller = models.ForeignKey(User, on_delete = models.CASCADE, related_name="seller")
     date_posted = models.DateTimeField(auto_now_add=True)
-    stock = models.PositiveIntegerField(default=0)
+    stock = models.DecimalField(max_digits=20, decimal_places=2, default=0.00, validators=[MinValueValidator(Decimal('0.00'))])
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
     img = models.ImageField(default = "default.png", upload_to = "product_imgs/", blank=True, null=True)
@@ -50,4 +50,32 @@ class Product(models.Model):
         return reverse('prod-detail', kwargs={'pk': self.pk})
     
 
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=3, decimal_places=2, default=0.00, validators=[MinValueValidator(Decimal('0.00'))])
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    phone_num = models.PositiveIntegerField(blank=True, null=True)
+    address1 = models.CharField(max_length = 255)
+    address2 = models.CharField(max_length = 255, blank=True, null=True)
+    country = models.CharField(max_length = 30)
+    state = models.CharField(max_length = 30)
+    zip_code = models.PositiveIntegerField()
+
     
+
